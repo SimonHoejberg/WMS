af0229219c318ddf1f53c15c2445948df2b52546
Jonas Alberg Ibrahim <jibrah14@student.aau.dk>
2015-10-28 13:56:14 +0100

    Added class UserIDBox. Created obj user_data_obj in coreSystem. Overall just worked on UserIDBox.

--- WMS/GUI/UserIDBox.cs
+++ WMS/GUI/UserIDBox.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMS;
 
 namespace WMS.GUI
 {
@@ -30,9 +31,7 @@ namespace WMS.GUI
 
         private void userConfirm_btn_Click(object sender, EventArgs e)
         {
-            
+            this.DialogResult = System.Windows.Forms.DialogResult.OK;
         }
-
-        
     }
 }
