dc82240e6884400bd2ec471f1985fb97b34e5923
Crimix <christofferm@live.dk>
2015-11-06 13:06:14 +0100

    padhad

--- WMS/GUI/Main.Designer.cs
+++ WMS/GUI/Main.Designer.cs
@@ -116,8 +116,8 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(118, 509);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
