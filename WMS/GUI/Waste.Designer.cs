d572ee3c8c31d267b00a9d5798617abdd1b31ca8
Crimix <christofferm@live.dk>
2015-11-13 09:03:31 +0100

    Waste

--- WMS/GUI/Waste.Designer.cs
+++ WMS/GUI/Waste.Designer.cs
@@ -43,9 +43,10 @@
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(897, 526);
+            this.button11.Location = new System.Drawing.Point(1196, 647);
+            this.button11.Margin = new System.Windows.Forms.Padding(4);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.Size = new System.Drawing.Size(100, 28);
             this.button11.TabIndex = 7;
             this.button11.Text = "Confirm";
             this.button11.UseVisualStyleBackColor = true;
@@ -53,9 +54,10 @@
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(816, 526);
+            this.button10.Location = new System.Drawing.Point(1088, 647);
+            this.button10.Margin = new System.Windows.Forms.Padding(4);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.Size = new System.Drawing.Size(100, 28);
             this.button10.TabIndex = 6;
             this.button10.Text = "Cancel";
             this.button10.UseVisualStyleBackColor = true;
@@ -65,9 +67,10 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(12, 23);
+            this.comboBox3.Location = new System.Drawing.Point(16, 28);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(227, 21);
+            this.comboBox3.Size = new System.Drawing.Size(301, 24);
             this.comboBox3.TabIndex = 5;
             this.comboBox3.SelectionChangeCommitted += new System.EventHandler(this.comboBox3_SelectionChangeCommitted);
             // 
@@ -75,27 +78,29 @@
             // 
             this.dataGridView6.AllowUserToAddRows = false;
             this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView6.Location = new System.Drawing.Point(12, 55);
+            this.dataGridView6.Location = new System.Drawing.Point(16, 68);
+            this.dataGridView6.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView6.Name = "dataGridView6";
-            this.dataGridView6.Size = new System.Drawing.Size(960, 446);
+            this.dataGridView6.Size = new System.Drawing.Size(1280, 549);
             this.dataGridView6.TabIndex = 4;
-            this.dataGridView6.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView6_CellValueChanged);
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.listBox1);
-            this.panel1.Location = new System.Drawing.Point(394, 208);
+            this.panel1.Location = new System.Drawing.Point(525, 256);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 142);
+            this.panel1.Size = new System.Drawing.Size(267, 175);
             this.panel1.TabIndex = 8;
             this.panel1.Visible = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(122, 114);
+            this.button1.Location = new System.Drawing.Point(163, 140);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Choose";
             this.button1.UseVisualStyleBackColor = true;
@@ -104,17 +109,20 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(200, 108);
+            this.listBox1.Size = new System.Drawing.Size(265, 132);
             this.listBox1.TabIndex = 0;
             this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(259, 23);
+            this.textBox1.Location = new System.Drawing.Point(345, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 9;
             this.textBox1.Text = "Item No";
             this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
@@ -122,9 +130,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(365, 21);
+            this.button2.Location = new System.Drawing.Point(487, 26);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 10;
             this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
@@ -132,9 +141,9 @@
             // 
             // Waste
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ClientSize = new System.Drawing.Size(1312, 690);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.panel1);
@@ -143,7 +152,7 @@
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.dataGridView6);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Waste";
             this.Text = "Waste";
             this.Load += new System.EventHandler(this.Waste_Load);
