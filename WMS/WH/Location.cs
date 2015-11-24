33264322d2b100fd74a13d66c1a44a8f0e5ea3a4
Crimix <christofferm@live.dk>
2015-11-24 13:48:17 +0100

    Fix for derp

--- WMS/WH/Location.cs
+++ WMS/WH/Location.cs
@@ -4,17 +4,15 @@ namespace WMS.WH
     public class Location
     {
         public string Id { get; }
-        public string Unit { get; }
         public int Shelf { get; }
         public int ShelfNo { get; }
         public string ItemNo { get; }
         public int Space { get; }
         public int Quantity { get; }
 
-        public Location(string id, string unit, int shelf, int shelfNo, string itemNo, int space, int quantity)
+        public Location(string id, int shelf, int shelfNo, string itemNo, int space, int quantity)
         {
             Id = id;
-            Unit = unit;
             Shelf = shelf;
             ShelfNo = shelfNo;
             ItemNo = itemNo;
