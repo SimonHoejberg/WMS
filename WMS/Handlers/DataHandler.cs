7d29aebe8d3bda7306db17be79838a529a6a635d
Simon Højberg <shajbe14@student.aau.dk>
2015-12-03 09:36:45 +0100

    Commented datahandler

--- WMS/Handlers/DataHandler.cs
+++ WMS/Handlers/DataHandler.cs
@@ -59,6 +59,10 @@ namespace WMS.Handlers
         /// <returns></returns>
         public List<LogItem> GetLog(string itemNo) => LogToList(itemNo);
 
+        /// <summary>
+        /// Returns a list of all items from information
+        /// </summary>
+        /// <returns></returns>
         public List<Item> InfoToList()
         {
             List<Item> temp = new List<Item>();
@@ -71,6 +75,10 @@ namespace WMS.Handlers
             return temp;
         }
 
+        /// <summary>
+        /// Returns a list of all users
+        /// </summary>
+        /// <returns></returns>
         public List<string> UserToList()
         {
             List<string> temp = new List<string>();
@@ -82,6 +90,10 @@ namespace WMS.Handlers
             return temp;
         }
 
+        /// <summary>
+        /// Returns a list of order no. 
+        /// </summary>
+        /// <returns></returns>
         public List<Order> OrderToList()
         {
             List<Order> temp = new List<Order>();
@@ -101,6 +113,10 @@ namespace WMS.Handlers
             return temp;
         }
 
+        /// <summary>
+        /// Returns a list of all locations
+        /// </summary>
+        /// <returns></returns>
         public List<Location> LocationToList()
         {
             List<Location> temp = new List<Location>();
@@ -112,6 +128,11 @@ namespace WMS.Handlers
             return temp;
         }
 
+        /// <summary>
+        /// Returns a list of the latest log for one item no
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <returns></returns>
         public List<LogItem> LogToList(string itemNo)
         {
             List<LogItem> temp = new List<LogItem>();
@@ -122,8 +143,21 @@ namespace WMS.Handlers
             }
             return temp;
         }
+
+        /// <summary>
+        /// Returns a datadapter containing a number of items either for information or log
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <param name="db"></param>
+        /// <param name="searchTerm"></param>
+        /// <returns></returns>
         public MySqlDataAdapter Search(string itemNo, string db, string searchTerm) => sql.Search(itemNo, db, searchTerm);
 
+        /// <summary>
+        /// Gets one item from an itemNo
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <returns></returns>
         public Item GetItemFromItemNo(string itemNo)
         {
             Item item = null;
@@ -135,46 +169,108 @@ namespace WMS.Handlers
             return item;        
         }
 
+        /// <summary>
+        /// Returns a mysqldataadapter containing one item
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <param name="db"></param>
+        /// <returns></returns>
         public MySqlDataAdapter GetDataFromItemNo(string itemNo, string db)
         {
             return sql.GetDataForItemNo(db,ITEM,itemNo);
         }
 
-        public MySqlDataAdapter GetDataFromOrderNo(string orderNo)
-        {
-            return sql.GetDataForItemNo(REGISTER_DB, ORDER, orderNo);
-        }
+        /// <summary>
+        /// Returns an order
+        /// </summary>
+        /// <param name="orderNo"></param>
+        /// <returns></returns>
+        public MySqlDataAdapter GetDataFromOrderNo(string orderNo) => sql.GetDataForItemNo(REGISTER_DB, ORDER, orderNo);
 
+        /// <summary>
+        /// Update the location database 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="newQuantity"></param>
+        /// <param name="newItem"></param>
         public void ItemMove(string id, string newQuantity, string newItem)
         {
             sql.moveItem(id, newQuantity, newItem);
         }
 
+        /// <summary>
+        /// Updates an items location in information 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="newLocation"></param>
         public void ItemMove(string item, string newLocation)
         {
             sql.moveItem(item, newLocation);
         }
 
+        /// <summary>
+        /// Updates location, log and information databases
+        /// </summary>
+        /// <param name="operaton"></param>
+        /// <param name="itemNo"></param>
+        /// <param name="description"></param>
+        /// <param name="quantity"></param>
+        /// <param name="user"></param>
+        /// <param name="operation"></param>
+        /// <param name="id"></param>
         public void ActionOnItem(char operaton, string itemNo, string description, string quantity, string user, string operation,string id)
         {
             sql.UpdateInfo(id,itemNo, quantity, operaton, description, operation, user);
         }
 
+        /// <summary>
+        /// Updates location, log and information databases
+        /// </summary>
+        /// <param name="operaton"></param>
+        /// <param name="itemNo"></param>
+        /// <param name="description"></param>
+        /// <param name="quantity"></param>
+        /// <param name="operation"></param>
+        /// <param name="id"></param>
+        public void ActionOnItem(char operaton, string itemNo, string description, string quantity, string operation, string id)
+        {
+            ActionOnItem(operaton, itemNo, description, quantity, core.UserName, operation, id);
+        }
+
+        /// <summary>
+        /// Inserts a move action into the log
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <param name="description"></param>
+        /// <param name="date"></param>
+        /// <param name="quantity"></param>
+        /// <param name="user"></param>
+        /// <param name="operation"></param>
         public void MoveActionOnItem(string itemNo, string description, string date, int quantity, string user, string operation)
         {
             sql.LogOperation(itemNo, description, date, user, operation, quantity);
         }
 
-        public void ActionOnItem(char operaton, string itemNo, string description, string quantity, string operation, string id)
-        {
-            ActionOnItem(operaton, itemNo, description, quantity, core.UserName, operation,id);
-        }
-
+        /// <summary>
+        /// Places an item in the location and updates the log and information
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="location"></param>
+        /// <param name="newQuantity"></param>
+        /// <param name="newItem"></param>
+        /// <param name="usage"></param>
+        /// <param name="orderNo"></param>
+        /// <param name="description"></param>
         public void PlaceItem(string id, string location, string newQuantity, string newItem, string usage,string orderNo,string description)
         {
             sql.PlaceItem(id, newQuantity, newItem, usage, location, orderNo, description, core.UserName, core.Lang.REGISTED);
         }
 
+        /// <summary>
+        /// Get how much an item is used
+        /// </summary>
+        /// <param name="itemNo"></param>
+        /// <returns></returns>
         public int GetUsage(string itemNo)
         {
             MySqlDataReader reader = sql.GetItemInfo(INFOMATION_DB, ITEM, itemNo);
@@ -186,6 +282,10 @@ namespace WMS.Handlers
             return usage;
         }
 
+        /// <summary>
+        /// Get max number of shelfes
+        /// </summary>
+        /// <returns></returns>
         public int GetMaxShelf()
         {
             MySqlDataReader reader = sql.GetMaxShelf();
@@ -202,6 +302,10 @@ namespace WMS.Handlers
             return res;
         }
 
+        /// <summary>
+        /// Get the max space
+        /// </summary>
+        /// <returns></returns>
         public int GetMaxSpace()
         {
             MySqlDataReader reader = sql.GetMaxSpace();
@@ -217,6 +321,7 @@ namespace WMS.Handlers
             return res;
         }
 
+        //Closes the connection to the server
         public void CloseConnectionToServer()
         {
             sql.CloseConnection();
